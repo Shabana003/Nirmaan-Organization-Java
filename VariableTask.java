d8519d40d90d6531ce448786965a79a026a57307
Shabana003 <145092324+Shabana003@users.noreply.github.com>
2024-12-16 17:05:14 +0530

    Day 1 Task2

---
+++ VariableTask.java
@@ -0,0 +1,42 @@
+class VariableTask{
+
+public static void main(String [] a){
+
+byte b = 123;
+
+System.out.println("byte :"+b);
+
+short s = 23456;
+
+System.out.println("short :"+s);
+
+int i = 3333;
+
+System.out.println("int :"+i);
+
+long l = 9456789657l;
+
+System.out.println("long :"+l);
+
+float f = 234.78f;
+
+System.out.println("float :"+f);
+
+double d = 345.999999d;
+
+System.out.println("double :"+d);
+
+char c = 'S';
+
+System.out.println("char :"+c);
+
+String name = "SHABANA";
+
+System.out.println("String :"+name);
+
+boolean bl = false;
+
+System.out.println("boolean :"+bl);
+}
+}
+
